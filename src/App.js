1b4fa8ee68adbfd8024943ffd133b900852280c0
Mateusz <m4.jankowski@gmail.com>
2023-11-11 00:08:40 +0100

    func: popup can be clicked and unclicked, restructured code a bit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Flag from "./components/Flag";
 import Map from "./components/Map";
-import Ornaments from "./components/Ornaments";
 
 function App() {
   let castles = [
@@ -19,27 +18,20 @@ function App() {
     "kish",
     "abismo_helmm",
     "vacceos",
-    "hierros",
     "outlaw_camp",
     "bastetania",
     "andosia",
   ];
   return (
     <div className="row mx-5 my-5">
-      <Ornaments location={"position-absolute top-0 start-0"} position={"topleft"}/>
-      <Ornaments location={"position-absolute top-0 end-0" }position={"topright"}/>
-      <Ornaments location={"position-absolute bottom-0 start-0"} position={"bottomleft"}/>
-      <Ornaments location={"position-absolute bottom-0 end-0"} position={"bottomright"}/>
       <div className="col col-lg-2 text-bg-secondary p-3 position-relative">
         This will be legend and clickable options for map overlay
       </div>
       <div className="col position-relative">
-        <div>
           <Map />
           {castles.map((castle) => (
             <Flag location={castle} />
           ))}
-        </div>
       </div>
     </div>
   );
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,9 +1,5 @@
-function Popup(props){
-    return(
-    <div>
-        <div className={props.location}>This will be popup</div>
-    </div>
-    )
+function Popup(props) {
+  return <div className={props.location}>This will be popup</div>;
 }
 
-export default Popup
+export default Popup;
