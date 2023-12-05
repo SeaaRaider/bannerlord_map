3deb585befb51127369aa8b3c2b932f9ae434b2b
Mac <mac@Macs-Air.Dlink>
2023-12-05 22:08:48 +0100

    style: changed map buttons style

--- src/App.js
+++ src/App.js
@@ -20,17 +20,19 @@ function App() {
   });
 
   return (
-    <div className="row mx-3 my-3">
-      <MapNavbar/>
-      <Menu visibility={visibility} setVisibility={setVisibility} />
-      <div className="col position-relative">
-        <Routes>
-          <Route path="/" element={<Map visibility={visibility}/>}/>
-          <Route path="/Gofurk" element={<GofurkMap visibility={visibility}/>}/>
-          <Route path="/SomeMap" element={<SomeMap visibility={visibility}/>}/>
-        </Routes>
+    // <div className="background">
+      <div className="row mx-0 my-2">
+        <MapNavbar/>
+        <Menu visibility={visibility} setVisibility={setVisibility} />
+        <div className="col position-relative">
+          <Routes>
+            <Route path="/" element={<Map visibility={visibility}/>}/>
+            <Route path="/Gofurk" element={<GofurkMap visibility={visibility}/>}/>
+            <Route path="/SomeMap" element={<SomeMap visibility={visibility}/>}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    // </div>
   );
 }
 
